e1b54e27e9d67f80a3931112449199fcecc75e3c
Justin Landry <32848504+JCBLandry@users.noreply.github.com>
2020-07-13 01:05:15 -0500

    Possible fix as well as connection to back end

---
+++ solar-mid/src copy/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./components/Login";
+import PrivateRoute from './components/PrivateRoute'
+import ItemPage from './components/ItemPage'
+// import "./styles.scss";
+
+function App() {
+
+  
+  return (
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={ItemPage} />
+        {/* 
+          Build a PrivateRoute component that will 
+          display BubblePage when you're authenticated 
+        */}
+        <PrivateRoute exact path='/ItemPage' component={ItemPage} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Item.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+// get items from online api
+// it's return a json file
+class Item extends Component {
+    constructor(props){
+        super(props);
+        // this.handleSubmit.bind(this)
+        this.state = {
+            error : null,
+            isLoaded : false,
+            items : []          
+        };
+    }
+  
+    componentDidMount(){
+        // I will use fake api from jsonplaceholder website
+        // this return 100 items 
+        fetch("https://solar-mid-be.herokuapp.com/api/magicitems")
+        .then( response => response.json())
+        .then(
+            // handle the result
+            (result) => {
+                this.setState({
+                    isLoaded : true,
+                    items : result
+                });
+            },
+
+            // Handle error 
+            (error) => {
+                this.setState({
+                    isLoaded: true,
+                    error
+                })
+            },
+        )
+    }
+  
+    render() {
+        const {error, isLoaded, items} = this.state;
+
+        if(error){
+            return <div>Error in loading</div>
+        }else if (!isLoaded) {
+            return <div>Loading ...</div>
+        }else{
+            return(
+                <div>
+                    
+      
+                    <ol className="item">
+                    {
+                        items.map(item => (
+                            <li key={item.id} align="start">
+                                <div>
+                                    <p>Name: {item.name}</p>
+                                    <p>Details: {item.details}</p>
+                                    <p>Realm: {item.realm}</p>
+                                    <p>Expires: {item.expires}</p>
+                                    <p>Written By: {item.writtenBy}</p>
+                                    <p>Date Written: {item.dateWritten}</p>
+                                    <p>Formal Space Total: {item.formalSpaceTotal}</p>
+                                    <p>Formal Space Remaining: {item.formalSpaceRemaining}</p>
+                                    <p>Last Player: {item.lastPlayer}</p>
+                                    <p>Hidden/Masked Detail: {item.hiddenDetail}</p>
+                                </div>
+                            </li>
+                        ))
+                    }
+                    </ol>
+                </div>
+            );
+        }
+      
+    }
+  }
+  
+  export default Item;
