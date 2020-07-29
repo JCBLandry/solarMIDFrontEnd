0260eb5933031997ad269bbf7e579a6b03097579
Justin Landry <32848504+JCBLandry@users.noreply.github.com>
2020-07-29 15:03:40 -0500

    More Styling Changes

--- src/components/Item.js
+++ src/components/Item.js
@@ -54,11 +54,11 @@ class Item extends Component {
                             <li className="" key={item.id} align="start">
                                 <div>
                                     <div className="bg-red-200 p-3 m-5">
-                                        <p>Name: {item.name}</p>
+                                        <p className="font-bold">Name: {item.name}</p>
                                         <p>Magic Item ID: {item.id}</p>
                                     </div>
                                     <div className="bg-white p-3 m-5">
-                                        <p>Details: {item.oppDesc}</p>
+                                        <p className="font-bold">Details: {item.oppDesc}</p>
                                         <p>Vessel Material: {item.vesselMat}</p>
                                         <p>Vessel Type: {item.vesselType}</p>
                                         <p>Enchantments: {item.enchantments}</p>
@@ -68,7 +68,7 @@ class Item extends Component {
                                         <p>Hidden/Masked Detail: {item.hiddenDetail}</p>
                                     </div>
                                     <div className="bg-blue-200 p-3 m-5">
-                                        <p>Formal Details:</p>
+                                        <p className="font-bold">Formal Details:</p>
                                         <p>Formal Space Total: {item.formalSpaceTotal}</p>
                                     <p>Formal Space Remaining: {item.formalSpaceRemaining}</p>
                                     <p>Earth Expires: {item.expirationDateEarth}</p>
@@ -77,7 +77,7 @@ class Item extends Component {
                                     <p>Socket Amount: {item.socketAmount}</p>
                                     </div>
                                     <div className="bg-yellow-200 p-3 m-5">
-                                        <p>Edit Details:</p>
+                                        <p className="font-bold">Edit Details:</p>
                                         <p>Last Edit: {item.lastEditBy}</p>
                                         <p>Reason for Edit: {item.lastEditReason}</p>
                                         <p>Last Edit Date: {item.lastEditDate}</p>
