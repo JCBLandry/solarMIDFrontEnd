b001b2833e494f1bbc5ec7e13c74c94c57b4ec2a
Justin Landry <32848504+JCBLandry@users.noreply.github.com>
2020-08-29 15:24:29 -0500

    More form woes

--- src/components/ItemInput.js
+++ src/components/ItemInput.js
@@ -1,188 +1,191 @@
-import React, { useState } from 'react';
-import { useFormFields } from "./useFormFields"
-import { FormGroup, FormControl } from "react-bootstrap";
-import axios from 'axios';
-
+import React, { useState } from "react";
+import { useFormFields } from "./useFormFields";
+import axios from "axios";
 
 const ItemInput = () => {
-  const [ display, setDisplay ] = useState("hidden")
+  let itemDeets = {
+    name: "",
+    oopDesc: "",
+    vesselMat: "",
+    vesselType: "",
+    enchantments: "",
+    createdDate: Date(),
+    expirationDateEarth: "",
+    expirationDateCelestial: "",
+    formalSpaceTotal: 0,
+    formalSpaceRemaining: 0,
+    socketID: 0,
+    socketAmount: 0,
+    writtenBy: "",
+    lastEditBy: "",
+    lastEditReason: "",
+    lastEditDate: Date(),
+    lastPlayer: "",
+    hiddenDetail: "",
+  };
+  const [display, setDisplay] = useState({ display: "hidden" });
 
-  const [fields, handleFieldChange ] = useFormFields(
-    {
-      name: "",
-      oopDesc: "",
-      vesselMat: "",
-      vesselType: "",
-      enchantments: "",
-      createdDate: Date(),
-      expirationDateEarth: "",
-      expirationDateCelestial: "",
-      formalSpaceTotal: 0,
-      formalSpaceRemaining: 0,
-      socketID: 0,
-      socketAmount: 0,
-      writtenBy: "",
-      lastEditBy: "",
-      lastEditReason: "",
-      lastEditDate: Date(),
-      lastPlayer: "",
-      hiddenDetail: ""
-    }
-  );
+  const { value, bind, reset } = useFormFields("");
 
-const handleSubmit = (e) => {
-  axios.post('https://solar-mid-be.herokuapp.com/api/magicitems', fields)
-    .then(function (response) {
-        console.log(response)
-    })
-    .catch(function (error) {
-        console.log(error)
-    }) 
-}
+  const check = (e) => {
+    console.log(value);
+  };
 
-    return (
-      
-      <div className="max-w-lg rounded overflow-hidden shadow-lg m-10 p-10 bg-gray-200">
-        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-blue-700 rounded " onClick={()=> setDisplay("block")}>New Magic Item</button>
-        <div className={display}>
-        <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-blue-700 rounded "onClick={()=> setDisplay("hidden")}>Cancel</button>
-        
+  const handleSubmit = (e) => {
+    e.preventdefault();
+    axios
+      .post("https://solar-mid-be.herokuapp.com/api/magicitems", value)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    alert(`Submitting Name ${value.name}`);
+  };
+
+  return (
+    <div className="max-w-lg rounded overflow-hidden shadow-lg m-10 p-10 bg-gray-200">
+      <button
+        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-blue-700 rounded "
+        onClick={() => check()}
+      >
+        Check Fields
+      </button>
+      <button
+        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-blue-700 rounded "
+        onClick={() => setDisplay({ display: "block" })}
+      >
+        New Magic Item
+      </button>
+      <div className={display}>
+        <button
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-blue-700 rounded "
+          onClick={() => setDisplay({ display: "hidden" })}
+        >
+          Cancel
+        </button>
+
         <p className="p-3 font-bold">New Magic Item</p>
         <form onSubmit={handleSubmit}>
-        
-        <FormGroup controlId="name">
-          <FormControl
-          className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal"
-            autoFocus
+          <input
+            className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal"
             placeholder="Item Name"
-            onChange={handleFieldChange}
+            type="text"
+            {...bind}
           />
-        </FormGroup>
-        <FormGroup controlId="oopDesc">
-          <FormControl
-          className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal"
+          <input
+            controlId="oopDesc"
+            className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal"
             autoFocus
             placeholder="OOP Description"
-            onChange={handleFieldChange}
+            {...bind}
           />
-        </FormGroup>
 
-        <FormGroup controlId="vesselMat">
-          <FormControl
-          className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal"
+          <input
+            controlId="vesselMat"
+            className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal"
             autoFocus
             placeholder="Vessel Material"
-            onChange={handleFieldChange}
+            {...bind}
           />
-        </FormGroup>
 
-        <FormGroup controlId="vesselType">
-          <FormControl
-          className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal"
+          <input
+            controlId="vesselType"
+            className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal"
             autoFocus
             placeholder="Vessel Type (Sword, Wand, Boots, etc.."
-            onChange={handleFieldChange}
+            {...bind}
           />
-        </FormGroup>
 
-        <FormGroup controlId="enchantments">
-          <FormControl
-          className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal"
+          <input
+            controlId="enchantments"
+            className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal"
             autoFocus
             placeholder="Enchantments"
-            onChange={handleFieldChange}
+            {...bind}
           />
-        </FormGroup>
 
-        <FormGroup controlId="expirationDateEarth">
-          <FormControl
-          className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal"
+          <input
+            controlId="expirationDateEarth"
+            className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal"
             autoFocus
             placeholder="Earth Expiration Date"
-            onChange={handleFieldChange}
+            {...bind}
           />
-        </FormGroup>
 
-        <FormGroup controlId="expirationDateCelestial">
-          <FormControl
-          className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal"
+          <input
+            controlId="expirationDateCelestial"
+            className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal"
             autoFocus
             placeholder="Celestial Expiration Date"
-            onChange={handleFieldChange}
+            {...bind}
           />
-        </FormGroup>
 
-        <FormGroup controlId="formalSpaceRemaining">
-          <FormControl
-          className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal"
+          <input
+            controlId="formalSpaceRemaining"
+            className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal"
             autoFocus
             placeholder="Formal Space Remaining"
-            onChange={handleFieldChange}
+            type="number"
+            {...bind}
           />
-        </FormGroup>
 
-        <FormGroup controlId="formalSpaceTotal">
-          <FormControl
-          className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal"
+          <input
+            controlId="formalSpaceTotal"
+            className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal"
             autoFocus
             placeholder="Formal Space Total"
-            onChange={handleFieldChange}
+            {...bind}
           />
-        </FormGroup>
 
-        <FormGroup controlId="socketID">
-          <FormControl
-          className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal"
+          <input
+            controlId="socketID"
+            className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal"
             autoFocus
             placeholder="Socket ID"
-            onChange={handleFieldChange}
+            {...bind}
           />
-        </FormGroup>
 
-        <FormGroup controlId="socketAmount">
-          <FormControl
-          className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal"
+          <input
+            controlId="socketAmount"
+            className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal"
             autoFocus
             placeholder="Socket Amount"
-            onChange={handleFieldChange}
+            {...bind}
           />
-        </FormGroup>
-        
-        <FormGroup controlId="writtenBy">
-          <FormControl
-          className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal"
+
+          <input
+            controlId="writtenBy"
+            className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal"
             autoFocus
             placeholder="Written By"
-            onChange={handleFieldChange}
+            {...bind}
           />
-        </FormGroup>
 
-        {/* <input onChange={handleFieldChange} type="text" placeholder="Last Editor" name="lastEditBy" className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal"/>
-
-        <input onChange={handleFieldChange} type="text" placeholder="Last Edit Reason" name="lastEditReason" className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal"/> */}
-
-        <FormGroup controlId="lastPlayer">
-          <FormControl
-          className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal"
+          <input
+            controlId="lastPlayer"
+            className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal"
             autoFocus
             placeholder="Last Player Owned"
-            onChange={handleFieldChange}
+            {...bind}
           />
-        </FormGroup>
 
-        <FormGroup controlId="hiddenDetail">
-          <FormControl
-          className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal"
+          <input
+            controlId="hiddenDetail"
+            className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal"
             autoFocus
             placeholder="Hidden Details (Cursed, Masked, Etc..)"
-            onChange={handleFieldChange}
+            {...bind}
           />
-        </FormGroup>
 
-        <input type="submit" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"/>
+          <input
+            type="submit"
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+          />
         </form>
-        </div>
+      </div>
     </div>
-    );
-}
-export default ItemInput
+  );
+};
+export default ItemInput;
--- src/components/useFormFields.js
+++ src/components/useFormFields.js
@@ -1,15 +1,32 @@
+// import { useState } from "react";
+
+// export function useFormFields(initialState) {
+//   const [fields, setValues] = useState(initialState);
+
+//   return [
+//     fields,
+//     function (e) {
+//       setValues({
+//         ...fields,
+//         [e.target.id]: e.target.value,
+//       });
+//     },
+//   ];
+// }
 import { useState } from "react";
 
-export function useFormFields(initialState) {
-  const [fields, setValues] = useState(initialState);
+export const useFormFields = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
 
-  return [
-    fields,
-    function (event) {
-      setValues({
-        ...fields,
-        [event.target.id]: event.target.value,
-      });
+  return {
+    value,
+    setValue,
+    reset: () => setValue(""),
+    bind: {
+      value,
+      onChange: (e) => {
+        setValue(e.target.value);
+      },
     },
-  ];
-}
+  };
+};
