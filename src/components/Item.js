bd1c33d276c76d2e5c01ab46d7433333b4baf787
Justin Landry <32848504+JCBLandry@users.noreply.github.com>
2021-01-18 16:43:59 -0600

    Revert "Merge pull request #9 from JCBLandry/New-Features"
    This reverts commit 97ddde928cec9f272cb3de011d2091664cf3d6a1, reversing
    changes made to db240422e773f451867a6b81ea3133ea95d154a8.

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,62 +1,64 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useFormFields } from "./useFormFields";
 import axios from "axios";
 // get items from online api
 // it's return a json file
 const Item = () => {
-  const [data, setData] = useState([]);
+  const [id, handleID] = useFormFields({
+    id: 1,
+  });
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios(
-        "https://solar-mid-be.herokuapp.com/api/magicitems"
-      );
-
-      setData(result.data);
-      console.log(data);
-    };
-
-    fetchData();
-  }, []);
+  const [items, setItems] = useState(
+    axios
+      .post("https://solar-mid-be.herokuapp.com/api/magicitems")
+      .then(function (response) {
+        return response;
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  );
 
   return (
-    <ul>
-      {data.map((item) => (
-        <li key={item.id}>
-          <div className="max-w-lg rounded overflow-hidden shadow-lg m-10 p-10 bg-gray-200">
-            <div className="bg-red-200 p-3 m-5">
-              <p className="font-bold">Name: {item.name}</p>
-              <p>Magic Item ID: {item.id}</p>
+    <div>
+      <ol className="item">
+        {items.map((item) => (
+          <li className="" key={item.id} align="start">
+            <div className="max-w-lg rounded overflow-hidden shadow-lg m-10 p-10 bg-gray-200">
+              <div className="bg-red-200 p-3 m-5">
+                <p className="font-bold">Name: {item.name}</p>
+                <p>Magic Item ID: {item.id}</p>
+              </div>
+              <div className="bg-white p-3 m-5">
+                <p className="font-bold">Details: {item.oppDesc}</p>
+                <p>Vessel Material: {item.vesselMat}</p>
+                <p>Vessel Type: {item.vesselType}</p>
+                <p>Enchantments: {item.enchantments}</p>
+                <p>Written by: {item.writtenBy}</p>
+                <p>Date Written: {item.createdDate}</p>
+                <p>Last Player: {item.lastPlayer}</p>
+                <p>Hidden/Masked Detail: {item.hiddenDetail}</p>
+              </div>
+              <div className="bg-blue-200 p-3 m-5">
+                <p className="font-bold">Formal Details:</p>
+                <p>Formal Space Total: {item.formalSpaceTotal}</p>
+                <p>Formal Space Remaining: {item.formalSpaceRemaining}</p>
+                <p>Earth Expires: {item.expirationDateEarth}</p>
+                <p>Celestrial Expires: {item.expirationDateCelestial}</p>
+                <p>Socket ID: {item.socketID}</p>
+                <p>Socket Amount: {item.socketAmount}</p>
+              </div>
+              <div className="bg-yellow-200 p-3 m-5">
+                <p className="font-bold">Edit Details:</p>
+                <p>Last Edit: {item.lastEditBy}</p>
+                <p>Reason for Edit: {item.lastEditReason}</p>
+                <p>Last Edit Date: {item.lastEditDate}</p>
+              </div>
             </div>
-            <div className="bg-white p-3 m-5">
-              <p className="font-bold">Details: {item.oppDesc}</p>
-              <p>Vessel Material: {item.vesselMat}</p>
-              <p>Vessel Type: {item.vesselType}</p>
-              <p>Enchantments: {item.enchantments}</p>
-              <p>Written by: {item.writtenBy}</p>
-              <p>Date Written: {item.createdDate}</p>
-              <p>Last Player: {item.lastPlayer}</p>
-              <p>Hidden/Masked Detail: {item.hiddenDetail}</p>
-            </div>
-            <div className="bg-blue-200 p-3 m-5">
-              <p className="font-bold">Formal Details:</p>
-              <p>Formal Space Total: {item.formalSpaceTotal}</p>
-              <p>Formal Space Remaining: {item.formalSpaceRemaining}</p>
-              <p>Earth Expires: {item.expirationDateEarth}</p>
-              <p>Celestrial Expires: {item.expirationDateCelestial}</p>
-              <p>Socket ID: {item.socketID}</p>
-              <p>Socket Amount: {item.socketAmount}</p>
-            </div>
-            <div className="bg-yellow-200 p-3 m-5">
-              <p className="font-bold">Edit Details:</p>
-              <p>Last Edit: {item.lastEditBy}</p>
-              <p>Reason for Edit: {item.lastEditReason}</p>
-              <p>Last Edit Date: {item.lastEditDate}</p>
-            </div>
-          </div>
-        </li>
-      ))}
-    </ul>
+          </li>
+        ))}
+      </ol>
+    </div>
   );
 };
 export default Item;
@@ -81,4 +83,4 @@ export default Item;
 //           });
 //         }
 //       );
-//   } */
+//   }
