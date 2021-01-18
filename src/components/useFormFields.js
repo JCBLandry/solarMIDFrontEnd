f9c56942add09ebaad272fba7a124ff798639fa9
Justin Landry <32848504+JCBLandry@users.noreply.github.com>
2021-01-18 16:47:04 -0600

    Revert "Revert "Bug Fix""
    This reverts commit cf5f2476c83681fce5759fd25a1dfc1dc1eca87c.

--- src/components/useFormFields.js
+++ src/components/useFormFields.js
@@ -1,32 +1,32 @@
-// import { useState } from "react";
-
-// export function useFormFields(initialState) {
-//   const [fields, setValues] = useState(initialState);
-
-//   return [
-//     fields,
-//     function (e) {
-//       setValues({
-//         ...fields,
-//         [e.target.id]: e.target.value,
-//       });
-//     },
-//   ];
-// }
 import { useState } from "react";
 
-export const useFormFields = (initialValue) => {
-  const [value, setValue] = useState(initialValue);
+export function useFormFields(initialState) {
+  const [fields, setValues] = useState(initialState);
 
-  return {
-    value,
-    setValue,
-    reset: () => setValue(""),
-    bind: {
-      value,
-      onChange: (e) => {
-        setValue(e.target.value);
-      },
+  return [
+    fields,
+    function (e) {
+      setValues({
+        ...fields,
+        [e.target.id]: e.target.value,
+      });
     },
-  };
-};
+  ];
+}
+// import { useState } from "react";
+
+// export const useFormFields = (initialValue) => {
+//   const [value, setValue] = useState(initialValue);
+
+//   return {
+//     value,
+//     setValue,
+//     reset: () => setValue(""),
+//     bind: {
+//       value,
+//       onChange: (e) => {
+//         setValue(e.target.value);
+//       },
+//     },
+//   };
+// };
